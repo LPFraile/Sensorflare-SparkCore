86972715f4146767c8ed6dc4394f9ec175bbfb8e
LPFraile <lidia_bas@hotmail.com>
2015-07-31 11:08:34 +0300

    Create sensorflare-relayboard.cpp

---
+++ firmware/examples/sensorflare-relayboard.cpp
@@ -0,0 +1,28 @@
+//Include the SensorFlare library 
+#include "sensorflare.h"
+
+//Initialize objects from the library
+//One object of the class "DigitalOut" is initialized for 
+//every digital output that will be remote control
+SensorFlare::DigitalOut outputPin1(D0);
+SensorFlare::DigitalOut outputPin2(D1);
+SensorFlare::DigitalOut outputPin3(D2);
+SensorFlare::DigitalOut outputPin4(D3);
+
+void setup() {
+    
+// Call the begin() functions for every object of the classes "DigitalOut" and 
+//"PWMout" to be wired up correct and available.
+    outputPin1.begin();
+    outputPin2.begin();
+    outputPin3.begin();
+    outputPin4.begin();
+
+}
+
+void loop() {
+    
+
+}
+
+
